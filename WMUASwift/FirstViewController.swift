7657e680ae60a5fd7758d48c7f1685cbd9f656fd
Mike Keegan <mikekeegan@1x-nat-vl932-172-30-234-252.wireless.umass.edu>
2016-03-30 16:35:16 -0400

    Initial Commit

---
+++ WMUASwift/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WMUASwift
+//
+//  Created by Mike Keegan on 3/30/16.
+//  Copyright © 2016 mikekeegan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
